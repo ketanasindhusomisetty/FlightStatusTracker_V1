58222e4fd149d850596243d018744075337a14e6
Ketana Sindhu <sindhusomishetty@gmail.com>
2026-06-11 14:17:44 +0530

    Added profile section and improved home page UI

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,22 +1,62 @@
-function Signup() {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Signup({ setIsLoggedIn, setUsername }) {
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+
+  const handleSignup = () => {
+
+    setUsername(name)
+setIsLoggedIn(true)
+
+localStorage.setItem("username", name)
+localStorage.setItem("isLoggedIn", "true")
+
+navigate("/")
+  };
+
   return (
     <div>
       <h1>Sign Up Page</h1>
 
       <form>
-        <input type="text" placeholder="Enter Name" />
+
+        <input
+          type="text"
+          placeholder="Enter Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
         <br /><br />
 
-        <input type="email" placeholder="Enter Email" />
+        <input
+          type="email"
+          placeholder="Enter Email"
+        />
+
         <br /><br />
 
-        <input type="password" placeholder="Create Password" />
+        <input
+          type="password"
+          placeholder="Create Password"
+        />
+
         <br /><br />
 
-        <button>Sign Up</button>
+        <button
+          type="button"
+          onClick={handleSignup}
+        >
+          Sign Up
+        </button>
+
       </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
